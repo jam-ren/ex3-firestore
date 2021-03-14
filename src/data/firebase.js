b4036bd90393f68feb53895a8c3668543c481dcf
Rena Jamaleddine <rena.jd12@gmail.com>
2021-03-14 14:33:10 -0400

    exercice fini

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,9 +4,13 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyCrAl_deR0fqGQ2BTp7x7-XtLog8vmy5o0",
+  authDomain: "pvt-2021.firebaseapp.com",
+  projectId: "pvt-2021",
+  storageBucket: "pvt-2021.appspot.com",
+  messagingSenderId: "181939174741",
+  appId: "1:181939174741:web:65d800cd36a9e2e94803dc"
 };
-
 // Initialiser Firebase
 if(!fbAppli.apps.length) {
   fbAppli.initializeApp(firebaseConfig);
@@ -16,3 +20,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
+export default bd;
