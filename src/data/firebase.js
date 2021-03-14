5062000f33f70ccc3f14a3c20f109e9e51b0d6db
Rena Jamaleddine <rena.jd12@gmail.com>
2021-03-14 14:39:16 -0400

    enlever config

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCrAl_deR0fqGQ2BTp7x7-XtLog8vmy5o0",
-  authDomain: "pvt-2021.firebaseapp.com",
-  projectId: "pvt-2021",
-  storageBucket: "pvt-2021.appspot.com",
-  messagingSenderId: "181939174741",
-  appId: "1:181939174741:web:65d800cd36a9e2e94803dc"
-};
+
 // Initialiser Firebase
 if(!fbAppli.apps.length) {
   fbAppli.initializeApp(firebaseConfig);
